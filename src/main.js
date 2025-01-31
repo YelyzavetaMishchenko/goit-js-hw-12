9cf5a0625a02aaabf69a3cff21a99cebcc7d3a46
Liza <yelyzaveta.mishchenko@gmail.com>
2025-01-31 18:21:57 +0200

    mist

--- src/main.js
+++ src/main.js
@@ -2,10 +2,15 @@ import { fetchImages } from './js/pixabay-api.js';
 import { renderGallery } from './js/render-functions.js';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
 const form = document.querySelector('#search-form');
 const gallery = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-more');
+const loader = document.createElement('div');
+loader.classList.add('loader');
+document.body.appendChild(loader);
 
 let query = '';
 let page = 1;
@@ -29,22 +34,57 @@ loadMoreBtn.addEventListener('click', async () => {
 
 async function searchImages() {
   try {
+    showLoader();
     const data = await fetchImages(query, page, perPage);
+    hideLoader();
+
     if (data.hits.length === 0) {
-      alert('No images found. Try a different search query.');
+      iziToast.error({
+        title: 'Error',
+        message:
+          'No images found for your query. Please try a different search.',
+        position: 'topRight',
+      });
       return;
     }
 
     gallery.insertAdjacentHTML('beforeend', renderGallery(data.hits));
     lightbox.refresh();
+    smoothScroll();
 
     if (page * perPage >= data.totalHits) {
       loadMoreBtn.classList.add('hidden');
-      alert("We're sorry, but you've reached the end of search results.");
+      iziToast.info({
+        title: 'End of results',
+        message: 'You have reached the end of the image search results.',
+        position: 'topRight',
+      });
     } else {
       loadMoreBtn.classList.remove('hidden');
     }
   } catch (error) {
-    alert('Something went wrong. Please try again later.');
+    hideLoader();
+    iziToast.error({
+      title: 'Error',
+      message: 'Something went wrong. Please try again later.',
+      position: 'topRight',
+    });
   }
+}
+
+function showLoader() {
+  loader.style.display = 'block';
+}
+
+function hideLoader() {
+  loader.style.display = 'none';
+}
+
+function smoothScroll() {
+  const { height: cardHeight } =
+    gallery.firstElementChild.getBoundingClientRect();
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
 }
